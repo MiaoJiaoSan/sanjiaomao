70ff4ee83b349c2d68bd65d154c658c35986f365
miaojiaosan <Magickey1.1>
2020-11-18 00:49:02 +0800

    RefreshToken

--- sanjiaomao-auth/src/main/resources/account.sql
+++
@@ -1,9 +0,0 @@
-create table account(
-    id bigint unsigned primary key auto_increment,
-    username varchar(24) not null comment '用户名',
-    password varchar(255) not null comment '密码',
-    enable boolean not null default true,
-    non_expired boolean not null default true,
-    non_locked  boolean not null default true,
-    credentials_non_expired boolean not null default true
-) charset = utf8mb4;
