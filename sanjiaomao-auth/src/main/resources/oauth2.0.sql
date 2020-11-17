4a3f2e268e23f588ba5b0fb4e152a926e572f8d2
miaojiaosan <Magickey1.1>
2020-11-18 00:53:59 +0800

    sql

--- sanjiaomao-auth/src/main/resources/oauth2.0.sql
+++ sanjiaomao-auth/src/main/resources/oauth2.0.sql
@@ -1,11 +1,12 @@
-create table account(
-    id bigint unsigned primary key auto_increment,
-    username varchar(24) not null comment '用户名',
-    password varchar(255) not null comment '密码',
-    enable boolean not null default true,
-    non_expired boolean not null default true,
-    non_locked  boolean not null default true,
-    credentials_non_expired boolean not null default true
+create table account
+(
+    id                      bigint unsigned primary key auto_increment,
+    username                varchar(24)  not null comment '用户名',
+    password                varchar(255) not null comment '密码',
+    enable                  boolean      not null default true,
+    non_expired             boolean      not null default true,
+    non_locked              boolean      not null default true,
+    credentials_non_expired boolean      not null default true
 ) charset = utf8mb4;
 
 create table oauth_access_token
@@ -74,3 +75,78 @@ create table oauth_refresh_token
     authentication blob         null comment 'OAuth2Authentication.java对象序列化后的二进制数据'
 )
     comment '更新令牌' charset = utf8;
+
+create table oauth_client_details
+(
+    client_id               VARCHAR(256) PRIMARY KEY,
+    resource_ids            VARCHAR(256),
+    client_secret           VARCHAR(256),
+    scope                   VARCHAR(256),
+    authorized_grant_types  VARCHAR(256),
+    web_server_redirect_uri VARCHAR(256),
+    authorities             VARCHAR(256),
+    access_token_validity   INTEGER,
+    refresh_token_validity  INTEGER,
+    additional_information  VARCHAR(4096),
+    autoapprove             VARCHAR(256)
+);
+
+create table oauth_client_token
+(
+    token_id          VARCHAR(256),
+    token LONGVARBINARY,
+    authentication_id VARCHAR(256) PRIMARY KEY,
+    user_name         VARCHAR(256),
+    client_id         VARCHAR(256)
+);
+
+create table oauth_access_token
+(
+    token_id          VARCHAR(256),
+    token LONGVARBINARY,
+    authentication_id VARCHAR(256) PRIMARY KEY,
+    user_name         VARCHAR(256),
+    client_id         VARCHAR(256),
+    authentication LONGVARBINARY,
+    refresh_token     VARCHAR(256)
+);
+
+create table oauth_refresh_token
+(
+    token_id VARCHAR(256),
+    token LONGVARBINARY,
+    authentication LONGVARBINARY
+);
+
+create table oauth_code
+(
+    code VARCHAR(256),
+    authentication LONGVARBINARY
+);
+
+create table oauth_approvals
+(
+    userId         VARCHAR(256),
+    clientId       VARCHAR(256),
+    scope          VARCHAR(256),
+    status         VARCHAR(10),
+    expiresAt      TIMESTAMP,
+    lastModifiedAt TIMESTAMP
+);
+
+
+-- customized oauth_client_details table
+create table ClientDetails
+(
+    appId                  VARCHAR(256) PRIMARY KEY,
+    resourceIds            VARCHAR(256),
+    appSecret              VARCHAR(256),
+    scope                  VARCHAR(256),
+    grantTypes             VARCHAR(256),
+    redirectUrl            VARCHAR(256),
+    authorities            VARCHAR(256),
+    access_token_validity  INTEGER,
+    refresh_token_validity INTEGER,
+    additionalInformation  VARCHAR(4096),
+    autoApproveScopes      VARCHAR(256)
+);
