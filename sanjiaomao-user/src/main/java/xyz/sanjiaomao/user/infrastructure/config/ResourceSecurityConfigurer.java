d2ad010df22951a25faa8c879d9fc6bf7b61666d
miaojiaosan <Magickey1.1>
2020-11-25 07:59:07 +0800

    registry

--- sanjiaomao-user/src/main/java/xyz/sanjiaomao/user/infrastructure/config/ResourceSecurityConfigurer.java
+++ sanjiaomao-user/src/main/java/xyz/sanjiaomao/user/infrastructure/config/ResourceSecurityConfigurer.java
@@ -1,13 +1,16 @@
 package xyz.sanjiaomao.user.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -41,7 +44,11 @@ public class ResourceSecurityConfigurer extends ResourceServerConfigurerAdapter 
   @Override
   public void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().anyRequest().authenticated();
+  }
 
+  @Autowired
+  public void remoteTokenServices(RemoteTokenServices remoteTokenServices, RestTemplate restTemplate){
+    remoteTokenServices.setRestTemplate(restTemplate);
   }
 
 
