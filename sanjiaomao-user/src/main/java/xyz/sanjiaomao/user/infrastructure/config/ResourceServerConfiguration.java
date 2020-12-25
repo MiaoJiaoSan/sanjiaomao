f7b69d7720bc6f2bb4a02919d31554c6a796ec41
miaojiaosan <Magickey1.1>
2020-12-25 20:53:53 +0800

    logFilter提前

--- sanjiaomao-user/src/main/java/xyz/sanjiaomao/user/infrastructure/config/ResourceServerConfiguration.java
+++ sanjiaomao-user/src/main/java/xyz/sanjiaomao/user/infrastructure/config/ResourceServerConfiguration.java
@@ -2,7 +2,6 @@ package xyz.sanjiaomao.user.infrastructure.config;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
