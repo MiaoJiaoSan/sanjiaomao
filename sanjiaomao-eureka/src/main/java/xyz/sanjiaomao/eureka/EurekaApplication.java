be0808bc4d33dd489119e93c8ced8853f6d3fa02
liyufei.lyf <liyufei.lyf@sunyur.com>
2020-12-27 22:31:00 +0800

    refresh

---
+++ sanjiaomao-eureka/src/main/java/xyz/sanjiaomao/eureka/EurekaApplication.java
@@ -0,0 +1,22 @@
+package xyz.sanjiaomao.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author 李宇飞
+ * create by 2020-12-27 21:15
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class,args);
+  }
+}
