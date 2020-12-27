b0b8be62b8130278bb17e4bc00daa63804d62f6b
liyufei.lyf <liyufei.lyf@sunyur.com>
2020-12-27 22:31:34 +0800

    refresh

--- sanjiaomao-user/src/main/java/xyz/sanjiaomao/user/UserApplication.java
+++ sanjiaomao-user/src/main/java/xyz/sanjiaomao/user/UserApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
